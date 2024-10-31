b3dd44280b4d6db529708a589264f4914453af08
Shiven Khurana <shivenbk@gmail.com>
2024-10-30 21:53:54 -0400

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,8 +11,6 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import Service from 'parts/Service';
 import Portfolio from 'parts/Portfolio';
-/*import Advantage from 'parts/Advantage';*/
-/*import Testimonial from 'parts/Testimonial';*/
 import Discuss from 'parts/Discuss';
 import Footer from 'parts/Footer';
 
